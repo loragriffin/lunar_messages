716ea60403583cf2589bfc8f6cf5966a6fb87728
Lora Griffin <lora.jean@me.com>
2017-12-15 21:13:14 -0600

    updated styling

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# lunar_messages
+# Lunar Messaging
 Simple message board app
+
+## Running app
+From the terminal, execute:
+```
+python manage.py runserver
+```
+
+Local server address will display. Navigate to 'localhost:8000' in your browser.
--- posts/form.py
+++ posts/form.py
@@ -6,7 +6,9 @@ from .models import Post
 class PostForm(forms.ModelForm):
     # overwrites model field with this custom form field
     # widget modifies how CharField is displayed
-    message = forms.CharField(max_length=500, min_length=5, widget=forms.Textarea)
+    message = forms.CharField(max_length=500, min_length=5,
+                              widget=forms.Textarea(attrs={'class': 'form-control'}))
+    user = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Post
