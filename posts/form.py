ef98c4180567ce328289b6f529997d47149c4bc2
Lora Griffin <lora.jean@me.com>
2017-12-18 10:26:56 -0600

    updated styling

--- posts/form.py
+++ posts/form.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
     # overwrites model field with this custom form field
     # widget modifies how CharField is displayed
     message = forms.CharField(max_length=500, min_length=5,
-                              widget=forms.Textarea(attrs={'class': 'form-control'}))
+                              widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
     user = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
