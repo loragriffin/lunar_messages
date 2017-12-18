60682878c73e3300bfcf901a8297a2897d48887a
Lora Griffin <lora.jean@me.com>
2017-12-18 15:20:39 -0600

    updated timestamp field

--- posts/form.py
+++ posts/form.py
@@ -6,9 +6,17 @@ from .models import Post
 class PostForm(forms.ModelForm):
     # overwrites model field with this custom form field
     # widget modifies how CharField is displayed
-    message = forms.CharField(max_length=500, min_length=5,
-                              widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '4'}))
-    user = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(
+        max_length=500,
+        min_length=5,
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': '4'
+        }))
+    user = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        }))
 
     class Meta:
         model = Post
--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     user = models.CharField(max_length=30)
     message = models.TextField()
-    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user
